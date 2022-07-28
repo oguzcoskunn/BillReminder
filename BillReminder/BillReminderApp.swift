4b07c115cff7e911f5976badaebcbf579fb0c3ec
Oguz Coskun <oguzcoskunn0@gmail.com>
2022-07-28 11:09:25 +0300

    Initial Commit

---
+++ BillReminder/BillReminderApp.swift
@@ -0,0 +1,20 @@
+//
+//  BillReminderApp.swift
+//  BillReminder
+//
+//  Created by Oğuz Coşkun on 28.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct BillReminderApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
